aaecb2b37929b04fbcf66bd1e43c883ad3532294
ANakesia28 <oliveiraanakesia70@gmail.com>
2026-08-11 11:12:01 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -30,3 +30,34 @@ areaMouse.addEventListener('mouseleave', function() {
     areaMouse.style.backgroundColor = "#ff0a0a";
     registrarLog("[MOUSE SAIU] Evento de mouse: Evento 'mouseleave' detectado!");
 });
+
+// evento de formulário:envio
+const meuFormulario = document.getElementById('meu-formulario');
+const campoInput = document.getElementById('campo-input');
+
+campoInput.addEventListener('input', function(evento) {
+    registrarLog(`[INPUT] Formulário: Evento 'input' disparado! -> Valor atual: "${campoInput.value}"`);
+});
+
+// evento de formulario: submit (acesso  ao evento de envio via id do formulario)
+// seuFormulario.addEventListener('submit', function(evento) {
+//    evento.preventDefault();
+//    registrarLog("[SUBMIT] Evento 'submit' detectado!");
+
+meuFormulario.addEventListener('submit', function( evento ) {
+     evento.preventDefault();
+    registrarLog("[FORMULÁRIO] Evento 'submit' detectado!");
+});
+
+//const campoEnvio = document.getElementById('campo-envio');
+//meuFormulario.addEventListener('submit', function( evento ) {
+    //evento.preventDefault();
+    //registraLog("[SUBMIT] Formulario: Evento 'submit' detectado!");
+//});
+
+
+const campoEnvio = document.getElementById('campo-envio');
+meuFormulario.addEventListener('submit', function( evento ) {
+    evento.preventDefault();
+    registraLog("[SUBMIT] Formulario: Evento 'submit' detectado!");
+});
